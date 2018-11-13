c003e13d4f109e2dfa6337394623788cf4543240
Louvivien <vivien.richaud@gmail.com>
2018-11-13 22:51:45 +0100

    test image

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,9 +14,6 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-config.serve_static_assets = true
-config.assets.compile = true
-  
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
@@ -31,7 +28,8 @@ config.assets.compile = true
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.serve_static_assets = true
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
